84f43a7c46f412874ebf54b7662e3d70c8c67e61
Luiz César Coppi Junior <jr.coppi@gmail.com>
2021-11-16 20:34:13 -0300

    adding new depedencies

--- src/main/java/com/jrcoppi/twitterSpring/TwitterSpringApplication.java
+++ src/main/java/com/jrcoppi/twitterSpring/TwitterSpringApplication.java
@@ -2,10 +2,7 @@ package com.jrcoppi.twitterSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-
-import com.jrcoppi.database.Post;
 
 
 @SpringBootApplication
